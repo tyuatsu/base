a8c886bf06ccd4ec5f51a55fd0ea7e3fb11bd4e1
andre <45877385+tyuatsu@users.noreply.github.com>
2024-04-02 18:02:15 -0300

    A arte de documentar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,20 +14,22 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    ...
-    'sphinx_rtd_theme',
-]
-
-html_theme = "sphinx_rtd_theme"
+#extensions = [
+#    ...
+#    'sphinx_rtd_theme',
+#]
+#
+#html_theme = "sphinx_rtd_theme"
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'pt_BR'
+language = 'pt'
+
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
+html_theme = 'alabaster'
+html_static_path = ['_static']
