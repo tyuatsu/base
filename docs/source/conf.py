c85b9aa5069cef472fb881a9875ccf8c97210a32
andre <45877385+tyuatsu@users.noreply.github.com>
2024-04-02 18:35:07 -0300

    A arte de documentar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    ...
-    'sphinx_rtd_theme',
-]
+extensions = ['sphinx_rtd_theme']
 html_theme = "sphinx_rtd_theme"
 
 sphinx-rtd-theme==7.2.6
