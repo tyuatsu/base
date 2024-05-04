f3e10b6c4d33e33bea69072ad0fcc3e4942a04df
andre <45877385+tyuatsu@users.noreply.github.com>
2024-05-04 17:41:20 -0300

    A arte de documentar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 # Required
-  install:
-    - requirements: docs/requirements.txt
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
